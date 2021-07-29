279a0bf712756d44b77cfbf27ae3e4fe748e7170
Shiyi Huang <3045712342@qq.com>
2021-07-29 22:57:07 +0800

    linux平台下的文件提交

---
+++ student.h
@@ -0,0 +1,21 @@
+//student.h
+#include <string>
+
+class Student
+{
+private:
+    /* data */
+    std::string name;
+    int age;
+public:
+    Student(/* args */);
+    ~Student();
+};
+
+Student::Student(/* args */)
+{
+}
+
+Student::~Student()
+{
+}
